7ecdbcec0ded9f79386656613b8f5f67c8be676e
Niels van der Weide <n.vanderweide@science.ru.nl>
2018-04-27 15:06:21 +0200

    Polynomial commute with set quotient (up to truncation)

--- setoid_prop.v
+++ setoid_prop.v
@@ -1,5 +1,5 @@
 Require Import HoTT.
-Require Import setoid.
+Require Import setoid polynomial.
 
 Definition squot {A : Type} (R : setoid A)
   : Type
@@ -37,6 +37,33 @@ Section set_is_setoid_quotient.
         apply path_ishprop.
   Defined.
 End set_is_setoid_quotient.
+
+Section path_setoid_quotient.
+  Variable (A : Type).
+    
+  Definition squot_to_TA : squot (path_setoid A) -> Trunc 0 A.
+  Proof.
+    simple refine (quotient_rec _ tr _).
+    cbn ; intros x y H.
+    simple refine (Trunc_rec _ H).
+    exact (ap tr).
+  Defined.
+
+  Global Instance squot_to_TA_isequiv : IsEquiv squot_to_TA.
+  Proof.
+    simple refine (isequiv_adjointify _ _ _ _).
+    - intros x.
+      simple refine (Trunc_rec _ x).
+      apply class_of.
+    - unfold Sect.
+      simple refine (Trunc_ind _ _) ; reflexivity.
+    - unfold Sect.
+      simple refine (quotient_ind _ _ _ _).
+      + reflexivity.
+      + intros.
+        apply path_ishprop.
+  Defined.
+End path_setoid_quotient.
 
 Section squot_sum.
   Variable (A B : Type)
@@ -151,3 +178,161 @@ Section squot_prod.
     := isequiv_adjointify _ squot_prod_in squot_prod_in_out_sect squot_prod_out_in_sect.
 End squot_prod.
 
+Definition Trunc_prod
+           {A B : Type}
+           (n : trunc_index)
+  : Trunc n A * Trunc n B -> Trunc n (A * B).
+Proof.
+  intros x.
+  simple refine (Trunc_rec _ (fst x)) ; intros y₁.
+  simple refine (Trunc_rec _ (snd x)) ; intros y₂.
+  exact (tr(y₁,y₂)).
+Defined.
+
+Definition Trunc_prod_inv
+           {A B : Type}
+           (n : trunc_index)
+  : Trunc n (A * B) -> Trunc n A * Trunc n B.
+Proof.
+  apply Trunc_rec.
+  exact (fun x => (tr (fst x), tr (snd x))).
+Defined.
+
+Global Instance Trunc_prod_isequiv
+       {A B : Type}
+       (n : trunc_index)
+  : IsEquiv (@Trunc_prod A B n).
+Proof.
+  simple refine (isequiv_adjointify _ (Trunc_prod_inv n) _ _) ; unfold Sect.
+  - simple refine (Trunc_ind _ _).
+    reflexivity.
+  - intros [x₁ x₂]. revert x₁.
+    simple refine (Trunc_ind _ _) ; intros y₁ ; simpl.
+    revert x₂.
+    simple refine (Trunc_ind _ _) ; intros y₂ ; simpl.
+    reflexivity.
+Defined.
+
+Global Instance Truncated_sum
+       {A B : Type}
+       (n : trunc_index)
+       (H : trunc_index_leq 0 n)
+       `{IsTrunc n A} `{IsTrunc n B}
+  : IsTrunc n (A + B).
+Proof.
+  apply trunc_sum' ; try apply _.
+  exact (trunc_leq H).
+Defined.
+
+Definition Trunc_sum
+           {A B : Type}
+           (n : trunc_index)
+  : Trunc n A + Trunc n B -> Trunc n (A + B).
+Proof.
+  intros [x | x].
+  - simple refine (Trunc_rec _ x) ; intros y.
+    exact (tr(inl y)).
+  - simple refine (Trunc_rec _ x) ; intros y.
+    exact (tr(inr y)).
+Defined.
+
+Definition Trunc_sum_inv
+           {A B : Type}
+           (n : trunc_index)
+           (H : trunc_index_leq 0 n)
+  : Trunc n (A + B) -> Trunc n A + Trunc n B.
+Proof.
+  simple refine (Trunc_rec _).
+  - apply (Truncated_sum n H).
+  - intros [x | x].
+    + exact (inl (tr x)).
+    + exact (inr (tr x)).
+Defined.
+
+Global Instance Trunc_sum_isequiv
+       {A B : Type}
+       (n : trunc_index)
+       (H : trunc_index_leq 0 n)
+  : IsEquiv (@Trunc_sum A B n).
+Proof.
+  simple refine (isequiv_adjointify _ (Trunc_sum_inv n H) _ _) ; unfold Sect.
+  - simple refine (Trunc_ind _ _).
+    intros [a | a] ; reflexivity.
+  - intros [x | x] ; revert x ; simple refine (Trunc_ind _ _).
+    * intros a.
+      pose (@Truncated_sum (Trunc n A) (Trunc n B) n H).
+      apply _.
+    * reflexivity.
+    * intros a.
+      pose (@Truncated_sum (Trunc n A) (Trunc n B) n H).
+      apply _.
+    * reflexivity.
+Defined.
+
+Definition pair_maps
+           {A B C D : Type}
+           (f : A -> C) (g : B -> D)
+  : A * B -> C * D
+  := fun z => (f (fst z), g (snd z)).
+
+Global Instance equiv_pair
+       {A B C D : Type}
+       (f : A -> C) (g : B -> D)
+       `{IsEquiv _ _ f} `{IsEquiv _ _ g}
+  : IsEquiv (pair_maps f g).
+Proof.
+  apply _.
+Defined.
+
+Definition sum_maps
+           {A B C D : Type}
+           (f : A -> C) (g : B -> D)
+  : A + B -> C + D
+  := fun z =>
+       match z with
+       | inl a => inl (f a)
+       | inr b => inr (g b)
+       end.
+
+Global Instance equiv_sum
+       {A B C D : Type}
+       (f : A -> C) (g : B -> D)
+       `{IsEquiv _ _ f} `{IsEquiv _ _ g}
+  : IsEquiv (sum_maps f g).
+Proof.
+  apply _.
+Defined.
+
+Definition poly_squot_to_squot
+           (P : polynomial)
+           {A : Type}
+           (R : setoid A)
+  : squot (lift_groupoid R P) -> Trunc 0 (poly_act P (squot R)).
+Proof.
+  induction P as [ | | P IHP Q IHQ | P IHP Q IHQ] ; cbn.
+  - exact tr.
+  - exact (squot_to_TA T).
+  - exact ((Trunc_prod 0)
+             o (pair_maps IHP IHQ)
+             o squot_prod_out _ _ _ _).
+  - exact ((Trunc_sum 0)
+              o (sum_maps IHP IHQ)
+              o squot_sum_out _ _ _ _).
+Defined.
+
+Global Instance squot_polynomial
+       (Q : polynomial)
+       {A : Type} `{IsHSet A}
+       (R : setoid A)
+       `{Funext}
+  : IsEquiv (poly_squot_to_squot Q R).    
+Proof.
+  induction Q.
+  - apply _.
+  - apply _.
+  - apply _.
+  - simpl.
+    refine isequiv_compose.
+    apply Trunc_sum_isequiv.
+    exact tt.
+Defined.
