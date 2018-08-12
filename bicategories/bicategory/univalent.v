beb74ca8b60f8995275f8920ecc3a571fcdfe408
Niels van der Weide <n.vanderweide@science.ru.nl>
2018-08-12 13:37:38 +0200

    Added definition of univalent bicategory

--- bicategories/bicategory/univalent.v
+++ bicategories/bicategory/univalent.v
@@ -1,10 +1,20 @@
 Require Import HoTT.
-From GR.bicategories Require Import general_category bicategory.bicategory.
+From GR.bicategories Require Import
+     general_category
+     bicategory.bicategory
+     bicategory.adjoint.
 
 Definition locally_univalent (C : BiCategory)
   : Type
   := forall (X Y : C), IsCategory (C⟦X,Y⟧).
 
-Global Instance locally_univalent_hprop `{Univalence} (C : BiCategory)
-  : IsHProp (locally_univalent C)
+Definition univalent_0 `{Univalence} (C : BiCategory)
+  : Type
+  := forall (X Y : C), IsEquiv(id_to_adjequiv X Y).
+
+Definition univalent `{Univalence} (C : BiCategory)
+  := (locally_univalent C * univalent_0 C)%type.
+
+Global Instance univalent_hprop `{Univalence} (C : BiCategory)
+  : IsHProp (univalent C)
   := _.
